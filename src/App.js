c6e044d057463a827a432b3c18d4e79968da5813
kkamara <kelvinkamara@protonmail.com>
2022-04-08 10:42:25 +0100

    Add basename to browser router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Routes from "./Routes"
 
 function App() {
   return (
-   <BrowserRouter>
+   <BrowserRouter basename={'/react-boilerplate/'}>
       <div className="App">
         <Routes/>
       </div>
