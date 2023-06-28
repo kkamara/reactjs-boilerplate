db1724ea3d47b37130e7cfcde784b5725f6fb5d8
kkamara <kamaracomputers@gmail.com>
2023-06-29 00:00:02 +0100

    Updates

---
+++ precache-manifest.828dbfbf250d6bdcf5f164f4d6218def.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89221ab9489dad652b9056e8945c70da",
+    "url": "/kelvinkamara/index.html"
+  },
+  {
+    "revision": "1fb85a89be5e5afb878b",
+    "url": "/kelvinkamara/static/css/2.81f49716.chunk.css"
+  },
+  {
+    "revision": "ea990262ca63e605c285",
+    "url": "/kelvinkamara/static/css/main.a115b5c1.chunk.css"
+  },
+  {
+    "revision": "1fb85a89be5e5afb878b",
+    "url": "/kelvinkamara/static/js/2.3781d6e2.chunk.js"
+  },
+  {
+    "revision": "b7b99439f8da9eed6d6a2d6f5237cb83",
+    "url": "/kelvinkamara/static/js/2.3781d6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150ab38b2d0a7a14e273",
+    "url": "/kelvinkamara/static/js/3.36a05895.chunk.js"
+  },
+  {
+    "revision": "1569a7d82a944de06977ceec9443f24f",
+    "url": "/kelvinkamara/static/js/3.36a05895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634e5e3f3c908a6aa0a8",
+    "url": "/kelvinkamara/static/js/4.de415502.chunk.js"
+  },
+  {
+    "revision": "781b39f146fcfcb5307d",
+    "url": "/kelvinkamara/static/js/5.3f2c312b.chunk.js"
+  },
+  {
+    "revision": "1f95d5965b3589d52209",
+    "url": "/kelvinkamara/static/js/6.feebd7e0.chunk.js"
+  },
+  {
+    "revision": "b98c82b001bb99e7d3ba",
+    "url": "/kelvinkamara/static/js/7.301ffe5e.chunk.js"
+  },
+  {
+    "revision": "57e0c86427f82491e07a",
+    "url": "/kelvinkamara/static/js/8.f6a06f2f.chunk.js"
+  },
+  {
+    "revision": "ed1d149946f4deae6f79",
+    "url": "/kelvinkamara/static/js/9.17c00f38.chunk.js"
+  },
+  {
+    "revision": "ea990262ca63e605c285",
+    "url": "/kelvinkamara/static/js/main.cecbfb72.chunk.js"
+  },
+  {
+    "revision": "9151f5a9449d1c7b2cd4",
+    "url": "/kelvinkamara/static/js/runtime-main.29999bd5.js"
+  },
+  {
+    "revision": "cbf9c83a4aea661d4c61e3272e5fe789",
+    "url": "/kelvinkamara/static/media/avatar-1.cbf9c83a.jpg"
+  },
+  {
+    "revision": "034d02d73fd503ee64391c76941432bc",
+    "url": "/kelvinkamara/static/media/avatar-2.034d02d7.jpg"
+  },
+  {
+    "revision": "7ce49759d33047eada521f43b0aa3294",
+    "url": "/kelvinkamara/static/media/avatar-3.7ce49759.jpg"
+  },
+  {
+    "revision": "cbb1401d069209edffd4ea178d9dcba0",
+    "url": "/kelvinkamara/static/media/logo.cbb1401d.svg"
+  }
+]);
