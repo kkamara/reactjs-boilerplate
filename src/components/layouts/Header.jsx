fa61824737f6f23e86e7b9667d2f9ec2918ab411
kkamara <kamaracomputers@gmail.com>
2023-12-19 11:35:57 +0000

    Update app name

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -32,7 +32,7 @@ export default function Header(props) {
 
   return <nav className="container navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="">PHP Reactjs Boilerplate</a>
+      <a className="navbar-brand" href="">Reactjs Boilerplate</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
