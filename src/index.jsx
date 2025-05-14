bd8cbea921e7e9d5fe5f180762768d0f286c51aa
kkamara <kamaracomputers@gmail.com>
2025-05-14 18:30:03 +0100

    Add favicon

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { createRoot, } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { Helmet } from 'react-helmet'
 
 import App from './App'
 import store from './redux/store'
 
 import './index.scss'
-import favicon from './favicon.png'
 
 import $ from'jquery/dist/jquery.min.js'
 import Popper from'@popperjs/core/dist/cjs/popper'
@@ -21,13 +19,6 @@ const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <Helmet>
-      <link 
-        rel="icon" 
-        type="image/png"
-        href={favicon}
-      />
-    </Helmet>
     <Provider store={store}>
       <App />
     </Provider>
